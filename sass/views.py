90c03da5b4c19a09372e202a53aa687f50025966
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2024-12-20 15:19:35 +0000

    second commit

--- sass/views.py
+++ sass/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse 
-
+import pathlib
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request,*args,**kwargs):
-    return HttpResponse("<h1>hello world</h1>")
+
+    html_ = """
+        <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>lewis is my name what is your name</h1>
+    </body>
+    </html>
+     """
+
+    return HttpResponse(html_)
